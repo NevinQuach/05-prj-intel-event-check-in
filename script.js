c3b54dff8636e55f5a06b4674f9d007be6706b26
Nevin Quach <nquach3@gmu.edu>
2026-02-20 01:55:53 +0000

    Commit

--- script.js
+++ script.js
@@ -1,37 +1,33 @@
 // Get all need information from the HTML
 const form = document.getElementById("checkInForm");
 const nameinput = document.getElementById("attendeeName");
-const teamselect = document.getElementById("teamSelect");
+const teaminput = document.getElementById("teamSelect");
 
 // Attendence count
 let count = 0;
 const maxCount = 50;
 
-// Form submission
+// Form
 form.addEventListener("submit", function (event) {
   event.preventDefault();
 
   // Form values
   const name = nameinput.value;
-  const team = teamselect.value;
-  const teamName = teamselect.selectedOptions[0].text;
-  console.log( name, team, teamName);
+  const team = teaminput.value;
+  const teamname = teaminput.selectedOptions[0].text;
 
   // Increase attendence count
   count++;
-  console.log("Total check ins: ", count);
 
   // Update progress bar
   const percentage = Math.round((count / maxCount) * 100) + "%";
-  console.log("Progress: " + percentage);
 
   // Update team count
   const teamcounter = document.getElementById(team + "Count");
   teamcounter.textContent = parseInt(teamcounter.textContent) +1;
 
   // Display welcome message
-  const message = `Welcome, ${name} from ${teamName}`;
-  console.log(message);
+  const message = `Welcome, ${name} from ${teamname}`;
 
   // Reset form
   form.reset();
